ac845fe14d66b6641b226190107bde8df0a879e4
aeyturak <136747568+aeyturak@users.noreply.github.com>
2026-01-02 19:00:17 +0300

    Delete Project/Services directory

--- Project/Services/clipboard_service.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-
-def copy_to_clipboard(text: str):
-
-    try:
-        QApplication.clipboard().setText(text)
-        
-    except Exception:
-        raise RuntimeError("Clipboard operation failed.")
